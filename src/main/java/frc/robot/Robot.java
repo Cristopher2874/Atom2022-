50ae2023d60675708b94f37ee13f61fc00ec3200
Jesus-e <58277848+Jesus-e@users.noreply.github.com>
2022-10-08 11:55:25 -0500

    pressureswithc smartdashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.controlboard.ControlBoard;
@@ -25,6 +26,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     mCompressor.enableDigital(); //habilita el compresor 
+    SmartDashboard.putBoolean("Pressure Switch:", mCompressor.getPressureSwitchValue());
     mLogsOutput.MasterLosgsOutputs(); //Manda llmar la funcion de Logs
   }
 
